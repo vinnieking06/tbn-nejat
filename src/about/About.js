491db4afd1a03b6462b36d427ceb028d5537555c
Vinnie <vincent.king1224@gmail.com>
2017-06-20 14:26:56 -0700

    made about more modular and added images to buttons

--- src/about/About.js
+++ src/about/About.js
@@ -9,51 +9,45 @@ import Founder from './Founder';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
  class HomeScreen extends React.Component {
+
   static navigationOptions = {
     title: 'About Us',
     tabBarIcon: () => (<Icon name="info-circle" size={30} color="white" />)
   };
 
+  constructor(props){
+    super(props);
+    this.changePage = this.changePage.bind(this);
+  }
+
+  changePage(page) {
+    const { navigate } = this.props.navigation;
+    navigate(page);
+  }
+
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-        
-        <TouchableOpacity onPress={() => navigate('Broadcaster')}>
-          <View style={styles.button}>
-            <Text style={styles.text}>Broadcasters</Text>
-            </View>
-          </TouchableOpacity>
-
-        <TouchableOpacity onPress={() => navigate('Schedule')}>
-          <View style={styles.button}>
-            <Text style={styles.text}>Schedule</Text>
-            </View>
-          </TouchableOpacity>
-
-        <TouchableOpacity onPress={() => navigate('Testimonies')}>
-          <View style={styles.button}>
-            <Text style={styles.text}>Testimonies</Text>
-            </View>
-          </TouchableOpacity>
-
-        <TouchableOpacity onPress={() => navigate('Founder')}>
-          <View style={styles.button}>
-            <Text style={styles.text}>Founder Info</Text>
-            </View>
-          </TouchableOpacity>
-
-        <TouchableOpacity onPress={() => navigate('Satellite')}>
-          <View style={styles.button}>
-            <Text style={styles.text}>Satellite</Text>
-            </View>
-          </TouchableOpacity>
-
+        <ButtonImage page={'Broadcaster'} changePage={this.changePage} image={'https://jpeg.org/images/jpeg2000-home.jpg'} />
+        <ButtonImage page={'Schedule'} changePage={this.changePage} image={'https://jpeg.org/images/jpegls-home.jpg'} />
+        <ButtonImage page={'Satellite'} changePage={this.changePage} image={'https://upload.wikimedia.org/wikipedia/commons/e/e6/Trees-sky.jpg'} />
+        <ButtonImage page={'Testimonies'} changePage={this.changePage} image={'https://jpeg.org/images/jpegls-home.jpg'} />
+        <ButtonImage page={'Founder'} changePage={this.changePage} image={'https://www.mycadsite.com/cad_08/tutorials/level_3/images/sky.jpg'} />
       </View>
     );
   }
 }
 
+const ButtonImage = (props) => {
+  return (
+       <TouchableOpacity style={{flex:1}} onPress={() => props.changePage(props.page)}>
+          <Image style = {styles.button} source={{uri: props.image}}>
+              <Text style={styles.text}>{props.page}</Text>
+          </Image>
+      </TouchableOpacity>
+  )
+}
 const About = StackNavigator({
   Home: { screen: HomeScreen},
   Broadcaster: { screen: BroadcasterContainer },
@@ -64,7 +58,7 @@ const About = StackNavigator({
   BroadcasterInfo: {screen: BroadcasterInfo},
   TestimonyInfo: {screen: TestimonyInfo}
 },{headerMode: 'screen', navigationOptions: {
-    headerTintColor: 'black', headerBackTitle: null, headerStyle: {backgroundColor: null, borderBottomWidth: 0}
+    headerTintColor: 'black', headerBackTitle: null, headerStyle: {backgroundColor: 'white', borderBottomWidth: 0}
 }});
 
 const styles = StyleSheet.create({
@@ -72,18 +66,19 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'stretch'
+    alignItems: 'stretch',
+    backgroundColor: 'rgba(0,0,0,0)'
   
   },
   button: {
+    flex: 1,
     alignItems: 'flex-start',
     justifyContent: 'center',
-    height: 112,
-    backgroundColor: 'white'
-    
+   
   },
   text: {
-    marginLeft: 10
+    marginLeft: 10,
+    color: 'white'
   }
 })
 
