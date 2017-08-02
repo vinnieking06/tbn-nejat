f0e3e529c218f701df236d5ec0613a6cd9e6332e
Vinnie <vincent.king1224@gmail.com>
2017-08-02 00:36:08 -0700

    updated ui in watch

--- src/Watch.js
+++ src/Watch.js
@@ -13,11 +13,13 @@ class Watch extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                    <View style={styles.section}>
-                        <Button style={{}} onPress={this.handlePress} color="pink" title="Watch Now"/>
-                        <Text>Watch Now</Text>
-                        <Text>Click here</Text>
+                 <TouchableOpacity style={styles.button}>
+                   <View>
+                        <Text style={styles.textBig}>Watch Now</Text>
+                        <Text style={styles.textSmall}>Click here</Text>
                     </View>
+                    <Icon style={styles.icon} name="arrow-circle-right" size={15} color='white' />
+                </TouchableOpacity> 
             </View>
         )
     }
@@ -30,15 +32,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  buttons: {
-    flexDirection: 'row'
-  },
   section: {
     flexDirection: 'column',
     width: 100,
     alignItems: 'center',
     justifyContent: 'center'
-  }
+  },
+  button: {
+    backgroundColor: 'blue',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    width: 150,
+    height: 50
+    },
+    textBig: {
+        color: 'white',
+        marginBottom: 3
+    },
+    textSmall: {
+        color: 'green',
+        fontSize: 12
+    },
+    icon: {
+        marginLeft: 15
+    }
 })
 
 export default Watch;
